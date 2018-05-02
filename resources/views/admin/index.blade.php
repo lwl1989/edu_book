dba8d97e9f03a2f9fdcaad8e88f6bfb744a0300a
li <285753421@qq.com>
2018-05-02 15:25:45 +0800

    webkit

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{csrf_token()}}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="renderer" content="webkit" />
     <meta name="force-rendering" content="webkit" />
     <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
